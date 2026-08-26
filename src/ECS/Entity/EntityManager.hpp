05dcdbb7ec5be7c90ec261808940eed7283a47ce
mathieuppicard <mathieuppicard@hotmail.com>
2026-08-25 20:37:34 -0400

    Redid everything, slowly adding up on top of Archetype now that I have a solid api

--- src/ECS/Entity/EntityManager.hpp
+++ src/ECS/Entity/EntityManager.hpp
@@ -5,116 +5,73 @@
 #include <unordered_map>
 #include <vector>
 #include <stdexcept>
+#include <typeindex>
 
 namespace ECS
 {
-    typedef uint8_t index_t; 
-    typedef index_t EntityId;
-    typedef index_t ComponentRegisterId;
-    typedef index_t ComponentId;
-    typedef index_t ArchetypeId;
-    typedef index_t StorageId;
+    typedef uint8_t index_t;
 
-    struct EntityManager;
-    
-    ////////////// COMPONENT SECTION //////////////
-    struct IComponentStorage{
-        virtual ~IComponentStorage() = default;
-
-        virtual void* get(size_t index) = 0;
-        virtual void addDefault() = 0;
-        virtual size_t size() = 0;
+    struct IStorage {
+        virtual ~IStorage() = default;
     };
 
     template<typename T>
-    struct ComponentStorage : IComponentStorage{
-        std::vector<T> data;
+    struct Storage : IStorage {
+        std::vector<T> storage;
 
-        void* get(size_t index) override { return &data[index]; }
-        void addDefault() override { data.emplace_back(); }
-        size_t size() override { return data.size(); }
-        template<typename... Args>
-        //To have acces to add to cast into ComponentStorage<T>
-        T& add(Args&&... args){
-            data.emplace_back(std::forward<Args>(args)...);
-            return data.back();
-        }
-    };
-
-    struct ComponentRegister {
-        ComponentRegisterId componentId;
-        std::unique_ptr<IComponentStorage> (*createStorage)();
-    };
-
-    ////////////// ARCHETYPE MAP //////////////
-    struct Archetype {
-        ArchetypeId archetypeId;
-        std::unordered_map<ComponentId, std::unique_ptr<IComponentStorage>> components;
-        bool has(index_t componentId){ return components.contains(componentId); }
-    };
-    
-    ////////////// Entity //////////////
-    struct Entity{
-        EntityId entityId;
-        EntityManager* manager;
-        std::vector<ComponentRegister> componentsToAdd;
-
-        void add(index_t id);
-    };
-
-    struct EntityMetada{
-        EntityId entityId;
-        // To try with archetypes unorder_map, if not work transform archetypes to vector
-        // Archetype* ptrArch;
-        ArchetypeId archetpyeId;
-        StorageId storageId;
-    };
-
-    ////////////// EntityManager //////////////
-    struct EntityManager
-    {
-        index_t nextComponentId = 0;
-        index_t nextEntityId = 0;
-        index_t nextArchetypeId = 0;
-
-        std::vector<ComponentRegister> componentRegistry;
-        std::unordered_map<EntityId,EntityMetada> entitiesMetadata;
-        std::unordered_map<ArchetypeId, Archetype> archetypes;
-
-        // Register the wanted components
-        void registerNewComponent(ComponentRegister newRegister);
-        bool isComponentRegister(index_t id);
-        ComponentRegister getComponentRegister(index_t id);
-
-        // Creating entity
-        void createEntity(Entity newEntity);
-        void addEntityToArchetype(Entity& entity, index_t archetypeId);
-        bool findingMatchingArchetype(std::vector<ComponentRegister>& compoRegistry, index_t* outIndex);
-        index_t createNewArchetype(Entity& entity);
-        
-        template<typename T>    //Templating function need to stay in .hpp so that the importing project can use it
-        void setValue(index_t entityId, index_t componentId ,T value){
-            EntityMetada md = entitiesMetadata.at(entityId);
-            if(archetype(md.archetpyeId).has(componentId)){
-                ComponentStorage<T>* storage = dynamic_cast<ComponentStorage<T>*>(archetype(md.archetpyeId).components.at(componentId).get());
-                T* data = static_cast<T*>(storage->get(md.storageId));
-                *data = std::move(value);
+        std::vector<T>* get() { return storage; }
+        T* at(index_t idx) { 
+            if(idx >= 0 && idx < storage.size()){ 
+                return &storage.at(idx); 
             }
+            return nullptr;
         }
-        
+        index_t count() { return storage.size(); }
+        void add(T data) { storage.push_back(std::move(data)); }
+        void remove(index_t idx) { storage.erase(storage.begin() + idx); }
+    };
+
+    struct Archetype{
+        std::unordered_map<std::type_index, std::unique_ptr<IStorage>> components;
+
         template<typename T>
-        ECS::ComponentStorage<T>* storeComponent(index_t archetypeId, index_t componentId){
-            if(archetype(archetypeId).has(componentId)){
-                return dynamic_cast<ECS::ComponentStorage<T>*>(
-                    archetype(archetypeId).components.at(componentId).get()
-                );
+        void add(T data){
+            auto it = components.find(typeid(T));
+            if(it == components.end()){
+                auto storagePtr = std::make_unique<Storage<T>>();
+                storagePtr->add(data);
+                components.emplace(typeid(T), std::move(storagePtr));
+            } else {
+                auto* storagePtr = static_cast<Storage<T>*>(it->second.get());
+                storagePtr->storage.push_back(std::move(data));
             }
-            throw std::runtime_error("Invalid component: " + std::to_string(componentId) + " for archetype: " + std::to_string(archetypeId)  + "\n");
         }
 
-        Archetype& archetype(index_t id);
-        index_t getNextArchetypeId() { return nextArchetypeId++; }
-        index_t getNextEntityId() { return nextEntityId++; }
-        index_t getNextComponentId(){ return nextComponentId++; }
-    }; 
+        template<typename T>
+        Storage<T>& getAllData(){
+            auto it = components.find(typeid(T));
+            if(it != components.end()){
+                return *static_cast<Storage<T>*>(it->second.get());
+            }
+            throw std::runtime_error("Invalid Component Type for this Archetype.");
+        }
+
+        template<typename T>
+        T* getData(index_t idx){
+            auto it = components.find(typeid(T));
+            if(it != components.end()){
+                return static_cast<Storage<T>*>(it->second.get())->at(idx);
+            }
+            throw std::runtime_error("Invalid Component Type for this Archetype.");          
+        }
+
+        template<typename T>
+        void remove(index_t idx){
+            getAllData<T>().remove(idx);           
+        }
+    };
+
+    struct EntityManager {
+        std::unordered_map<index_t,Archetype> archetypes;
+    };
 }
