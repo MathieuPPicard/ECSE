247cc0b1d2950f2482efcfe86c02d247fbdb3654
mathieuppicard <mathieuppicard@hotmail.com>
2026-08-30 11:53:14 -0400

    Creating entity, modifying data, adding component to entity

--- src/ECS/Entity/Archetype.hpp
+++ src/ECS/Entity/Archetype.hpp
@@ -13,39 +13,91 @@ namespace ECS
 {   
     struct IStorage {
         virtual ~IStorage() = default;
+
+        virtual void moveElementTo(index_t idx, IStorage& destination) = 0;
+        virtual std::unique_ptr<IStorage> moveElement(index_t idx) = 0;
+        virtual void remove(index_t idx) = 0;
+        virtual index_t count() = 0;
     };
 
     template<typename T>
     struct Storage : IStorage {
         std::vector<T> storage;
 
-        std::vector<T>* get() { return storage; }
+        void moveElementTo(index_t idx, IStorage& destination) override {
+            auto& typedDestination = dynamic_cast<Storage<T>&>(destination);
+
+            typedDestination.storage.emplace_back(
+                std::move(storage.at(idx))
+            );
+        }
+        
+        std::unique_ptr<IStorage> moveElement(index_t idx) override{
+            auto result = std::make_unique<Storage<T>>();
+            result->storage.emplace_back(std::move(storage.at(idx)));
+            return result;
+        }
+
+        void remove(index_t idx) override{
+            storage.erase(storage.begin() + idx); 
+        }
+        
+        index_t count() override { return storage.size(); }
+
         T* at(index_t idx) { 
             if(idx >= 0 && idx < storage.size()){ 
                 return &storage.at(idx); 
             }
             return nullptr;
         }
-        index_t count() { return storage.size(); }
+        std::vector<T>* get() { return storage; }
         void add(T data) { storage.push_back(std::move(data)); }
-        void remove(index_t idx) { storage.erase(storage.begin() + idx); }
     };
 
     struct Archetype{
         std::unordered_map<std::type_index, std::unique_ptr<IStorage>> components;
 
-        template<typename T>
-        void add(T data){
-            auto it = components.find(typeid(T));
-            if(it == components.end()){
-                auto storagePtr = std::make_unique<Storage<T>>();
-                storagePtr->add(data);
-                components.emplace(typeid(T), std::move(storagePtr));
-            } else {
-                auto* storagePtr = static_cast<Storage<T>*>(it->second.get());
-                storagePtr->storage.push_back(std::move(data));
+        template <typename T>
+        index_t addOne(T&& data) {
+            using Component = std::decay_t<T>;
+
+            auto it = components.find(typeid(Component));
+
+            if (it == components.end()) {
+                auto storage = std::make_unique<Storage<Component>>();
+                storage->add(std::forward<T>(data));
+
+                components.emplace(typeid(Component), std::move(storage));
+                return 0;
             }
+
+            auto* storage = static_cast<Storage<Component>*>(it->second.get());
+            storage->storage.push_back(std::forward<T>(data));
+
+            return storage->storage.size() - 1;
         }
+
+        template <typename... Ts>
+        index_t add(Ts&&... values) {
+            index_t position = 0;
+            bool hasPosition = false;
+
+            auto addAndCheckPosition = [&](auto&& value) {
+                const index_t newPosition =
+                    addOne(std::forward<decltype(value)>(value));
+
+                // All storages in an archetype must stay aligned.
+                if (hasPosition && newPosition != position) {
+                    throw std::logic_error("Archetype component storages are misaligned");
+                }
+
+                position = newPosition;
+                hasPosition = true;
+            };
+
+            (addAndCheckPosition(std::forward<Ts>(values)), ...);
+            return position;
+        }        
 
         template<typename T>
         Storage<T>& getAllData(){
@@ -57,10 +109,10 @@ namespace ECS
         }
 
         template<typename T>
-        T* getData(index_t idx){
+        T& getData(index_t idx){
             auto it = components.find(typeid(T));
             if(it != components.end()){
-                return static_cast<Storage<T>*>(it->second.get())->at(idx);
+                return static_cast<Storage<T>*>(it->second.get())->storage.at(idx);
             }
             throw std::runtime_error("Invalid Component Type for this Archetype.");          
         }
@@ -75,6 +127,11 @@ namespace ECS
             return components.contains(typeid(T));
         }
 
+        template<typename Component>
+        void set(index_t pos, Component&& value){
+            getData<Component>(pos) = std::forward<Component>(value);
+        }
+
         std::vector<std::type_index> getSignature(){
             std::vector<std::type_index> signature;
             int size = components.size();
@@ -83,6 +140,24 @@ namespace ECS
             }
             std::sort(signature.begin(),signature.end());
             return signature;
+        }
+    
+        index_t size(){
+            // Get the size of any storage(component) as they all have the same size
+            if(components.empty()){
+                return 0;
+            }
+            return components.begin()->second->count();
+        }
+
+        void debugPrintStorageSizes() const {
+            std::cout << "Archetype storage sizes:\n";
+
+            for (const auto& [type, storage] : components) {
+                std::cout << "  " << type.name()
+                        << ": " << storage->count()
+                        << '\n';
+            }
         }
     };
 
--- src/ECS/Entity/EntityManager.hpp
+++ src/ECS/Entity/EntityManager.hpp
@@ -15,7 +15,7 @@ namespace ECS
         index_t nextEntityId = 0;
         index_t nextArchetypeId = 0;
         std::unordered_map<index_t, Archetype> archetypes;
-        std::vector<Entity> entityMetadata;
+        std::unordered_map<index_t, Entity> entityMetadata;
 
         // Get all the archetype that contains the Component 
         template<typename T>
@@ -72,50 +72,131 @@ namespace ECS
         }
 
         template<typename... Components>
-        void createEntity(Components... components) 
+        index_t createEntity(Components... components) 
         {
+            index_t storePos = 0;
+
             std::vector<std::type_index> newSignature {
                 typeid(Components)...
             };
-            Archetype* archPtr = archetypeExists(newSignature);
-            if(archPtr != nullptr){
+            Archetype* archPtr = archetypeExists(&newSignature);
+            if(archPtr == nullptr){
                 // Create the new Archetype
                 Archetype newArchetype;
-                newArchetype.add<Components...>(components...);
+                storePos = newArchetype.add(std::forward<Components>(components)...);
                 index_t nextArchId = getNextArchetypeId();
                 archetypes[nextArchId] = std::move(newArchetype);
                 archPtr = &archetypes.at(nextArchId);
-
             } else {
                 // Add components to the already existing Archetype
-                archPtr->add<Components...>(components...);
+                storePos = archPtr->add(std::forward<Components>(components)...);
             }
 
-            index_t storePos = newSignature.size() - 1;
-
             // Create Entity(metadata) and add to entityMetadata
-            // not even sure if necessary
-            addMetaData(archPtr, storePos);
+            return addMetaData(archPtr, storePos);
         }
 
-        Archetype* archetypeExists(std::vector<std::type_index> signature){
-            // Iterate through each Archetype
-            // sort signature param
-            // archetype.getSignature() -> sorted list of component
-            // compare getSignature() and signature from param
-            // If not same size return nullptr
-            // else if one is not equal return nullptr
-            // else if all have an equal return the ptr to the archetype
-            // TODO
+        template<typename Component>
+        void set(Entity entity, Component&& value){
+            entity.archetypePtr->getData<Component>(entity.storageIdx) = std::forward<Component>(value);
+        }
 
+        template<typename T>
+        void addComponent(Entity& entity, T&& data){
+            Archetype* oldArchetype = entity.archetypePtr;
+
+            // Archetype/Entity already have this component
+            if(oldArchetype->has<T>()){
+                throw std::logic_error("Archetype/Entity already got component.");
+            }
+
+            // Adding to the signature of the entity
+            std::vector<std::type_index> signature = oldArchetype->getSignature();
+            std::type_index newCompoSignature = typeid(T);
+            signature.push_back(newCompoSignature);
+
+            index_t newPos = 0;
+            Archetype* newArchetypePtr = archetypeExists(&signature);
+            // Verify if the new signature already exist or not
+            if(newArchetypePtr != nullptr){
+                // Push the data in the already existing storage for each component
+                for(auto& [id,component] : oldArchetype->components){
+                    auto destinationIt = newArchetypePtr->components.find(id);
+                    if (destinationIt == newArchetypePtr->components.end()) {
+                        throw std::logic_error("Destination archetype is missing an existing component storage.");
+                    }
+                    component->moveElementTo(
+                        entity.storageIdx,
+                        *destinationIt->second
+                    );
+                }
+                newPos = newArchetypePtr->add<T>(std::move(data));
+            } else {
+                Archetype newArchetype;
+
+                for(auto& [id, component] : oldArchetype->components){
+                    newArchetype.components.emplace(
+                        id,
+                        component->moveElement(entity.storageIdx)
+                    );
+                }
+                // Add the new component
+                newPos = newArchetype.add<T>(std::move(data));
+
+
+                // Add the new archetype into archetypes
+                index_t nextArchId = getNextArchetypeId();
+                archetypes[nextArchId] = std::move(newArchetype);
+                newArchetypePtr = &archetypes.at(nextArchId);
+            }
+
+            index_t oldStorPos = entity.storageIdx;
+            // Modify metadata information with new information
+            entity.archetypePtr = newArchetypePtr;
+            entity.storageIdx = newPos;
+
+           for(auto& [id, component] : oldArchetype->components){
+                component->remove(oldStorPos);
+           }
+        }
+
+        template<typename T>
+        void removeComponent(Entity entity){
+            //TODO
+        }
+
+        ECS::Entity* entity(index_t entityIdx){
+            return &entityMetadata.at(entityIdx);
+        }
+
+        Archetype* archetypeExists(std::vector<std::type_index>* newSign){
+            // Sort the unordered signature
+            std::sort(newSign->begin(),newSign->end());
+            
+            // For each already exisitng archetype
+            for(auto& [id,archetype] : archetypes){
+                bool allMatched = true;
+                // Not same amount of component
+                if(newSign->size() != archetype.components.size()){
+                    continue;
+                }
+                std::vector<std::type_index> archSignature = archetype.getSignature();
+                if(*newSign == archSignature){
+                    return &archetype;
+                }
+            }
             return nullptr;
         }
 
-        // not even sure if necessary
-        void addMetaData(Archetype* archPtr, index_t storagePos){
-            // Create Entity struct with info given from createEntity
-            // std::move into entityMetadata vector
-            // TODO
+        index_t addMetaData(Archetype* archPtr, index_t storagePos){
+
+            index_t entityId = getNextEntityId();
+            entityMetadata[entityId] = std::move(Entity{
+                .entityId = entityId,
+                .archetypePtr= archPtr,
+                .storageIdx = storagePos
+            });
+            return entityId;
         }
 
         index_t getNextEntityId() { return nextEntityId++; }
